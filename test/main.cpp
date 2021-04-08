bd476208ed960118a58478b9f71a2d5dfcf04c75
RafalSzyinski1 <259380@student.pwr.edu.pl>
2021-04-08 02:35:58 +0200

    init

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,11 @@
 //
 // Created by rafal on 05.04.2021.
 //
+/*
+    "It isn't enough to think outside the box.
+    Thinking is passive.
+    Get used to acting outside the box."
+*/
 
 #include "gtest/gtest.h"
 
